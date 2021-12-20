ed60ca78284d2bccbd76ddc746ff3a4ef3db3d9c
Tuan Nguyen <quoctuan143@gmail.com>
2021-12-20 09:11:39 +0700

    update

---
+++ APP_HOATHO/APP_HOATHO/Dialog/AppInformation.xaml.cs
@@ -0,0 +1,44 @@
+﻿using APP_HOATHO.Global;
+using Rg.Plugins.Popup.Extensions;
+using Rg.Plugins.Popup.Pages;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace APP_HOATHO.Dialog
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AppInformation : PopupPage
+    {
+        TaskCompletionSource<DialogReturn> _tsk = null;
+        public AppInformation()
+        {
+            InitializeComponent();
+            lblHeDieuHanh.Text = Device.RuntimePlatform;
+            lblPhienBan.Text = AppInfo.VersionString;
+        }
+        public async Task<DialogReturn> Show()
+        {
+            _tsk = new TaskCompletionSource<DialogReturn>();
+            await Navigation.PushPopupAsync(this);
+            return await _tsk.Task;
+        }
+
+        private async void btnOK_Clicked(object sender, EventArgs e)
+        {
+            string url = string.Empty;
+            var location = RegionInfo.CurrentRegion.Name.ToLower();
+            if (Device.RuntimePlatform == Device.Android)
+                url = "https://play.google.com/store/apps/details?id=com.companyname.app_hoatho";
+            else if (Device.RuntimePlatform == Device.iOS)
+                url = "https://itunes.apple.com/" + location + "/app/HoaTho/id1560268571?mt=8";
+            await Browser.OpenAsync(url, BrowserLaunchMode.External);
+        }
+    }
+}
---
+++ APP_HOATHO/APP_HOATHO/Dialog/MessageChangePassword.xaml.cs
@@ -0,0 +1,84 @@
+﻿using APP_HOATHO.Global;
+using Rg.Plugins.Popup.Extensions;
+using Rg.Plugins.Popup.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace APP_HOATHO.Dialog
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MessageChangePassword : PopupPage
+    {
+        TaskCompletionSource<DialogReturn> _tsk = null;
+        public MessageChangePassword()
+        {
+            InitializeComponent();
+        }
+        private async void btnOK_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(oldPass.Text))
+            {
+                await new MessageBox("Nhập mật khẩu cũ").Show();
+                return;
+            }
+            if (string.IsNullOrEmpty(newPass.Text))
+            {
+                await new MessageBox("Nhập mật khẩu mới").Show();
+                return;
+            }
+            if (string.IsNullOrEmpty(newPass1.Text))
+            {
+                await new MessageBox("Nhập lại mật khẩu mới").Show();
+                return;
+            }
+            if (newPass.Text != newPass1.Text)
+            {
+                await new MessageBox("Mật khẩu mới nhập lại không trùng nhau").Show();
+                return;
+            }
+
+            ChangePasswordModel change = new ChangePasswordModel { Username = Preferences.Get(Config.PhoneNumber, ""), OldPassword = oldPass.Text, NewPassword = newPass.Text };
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Config.URL);
+                var ok = client.PostAsJsonAsync("api/changepass", change);
+                if (ok.Result.Content.ReadAsStringAsync().Result.ToLower().Contains("true"))
+                {
+                    await new MessageBox("Đổi mật khẩu thành công").Show();
+                    await Navigation.PopPopupAsync(true);
+                    _tsk.SetResult(DialogReturn.OK);
+                }
+                else
+                {
+                    await new MessageBox(ok.Result.Content.ReadAsStringAsync().Result.Replace("\"", "")).Show();
+                }
+            }
+
+        }
+
+        private async void btnCancel_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopPopupAsync(true);
+            _tsk.SetResult(DialogReturn.Cancel);
+        }
+        public async Task<DialogReturn> Show()
+        {
+            _tsk = new TaskCompletionSource<DialogReturn>();
+            await Navigation.PushPopupAsync(this);
+            return await _tsk.Task;
+        }
+    }
+    public class ChangePasswordModel
+    {
+        public string Username { get; set; }
+        public string OldPassword { get; set; }
+        public string NewPassword { get; set; }
+    }
+}
