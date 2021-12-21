9aa9ffac7f11ec705c9240b649085b3826be1d98
Tuan Nguyen <quoctuan143@gmail.com>
2021-12-21 18:09:43 +0700

    update

--- APP_HOATHO/APP_HOATHO/ViewModels/AboutViewModel.cs
+++ APP_HOATHO/APP_HOATHO/ViewModels/AboutViewModel.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using APP_HOATHO.Dialog;
+using APP_HOATHO.Global;
+using APP_HOATHO.Views;
+using System;
 using System.Windows.Input;
 using Xamarin.Essentials;
 using Xamarin.Forms;
@@ -7,12 +10,62 @@ namespace APP_HOATHO.ViewModels
 {
     public class AboutViewModel : BaseViewModel
     {
+        #region "Field"
+
+        INavigation Navigation;
+        #endregion
+
+        #region "Contructor"
+
         public AboutViewModel()
         {
-            Title = "About";
-            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://xamarin.com"));
+            Title = "Thông tin ứng dụng";
+
+            LogoutCommand = new Command(OnLogoutClicked);
+            ChangePasswordCommand = new Command(OnChangePassClicked);
+            InformationCommand = new Command(OnShowInformationClicked);
         }
 
-        public ICommand OpenWebCommand { get; }
+
+
+
+
+        #endregion
+
+        #region "Method"
+        private void OnLogoutClicked(object obj)
+        {
+            App.Current.MainPage = new Login();
+        }
+        private async void OnChangePassClicked(object obj)
+        {
+            try
+            {
+                var ok = await new MessageChangePassword().Show();
+                if (ok == DialogReturn.OK)
+                {
+                    App.Current.MainPage = new Login();
+                }
+            }
+            catch (Exception)
+            {
+
+
+            }
+        }
+
+        private async void OnShowInformationClicked(object obj)
+        {
+            await new AppInformation().Show();
+        }
+
+        #endregion
+
+        #region "Command"
+        public Command InformationCommand { get; }
+        public Command LogoutCommand { get; }
+        public Command ChangePasswordCommand { get; }
+        #endregion
+
     }
 }
