6fcd71eef7e81cb94684d7465b24bd59a7618d0c
Tuan Nguyen <quoctuan143@gmail.com>
2021-12-22 09:50:29 +0700

    update

--- APP_HOATHO/APP_HOATHO/Views/Main.xaml.cs
+++ APP_HOATHO/APP_HOATHO/Views/Main.xaml.cs
@@ -39,10 +39,10 @@ namespace APP_HOATHO.Views
         public int NofiLichXich { get; set; }
         public int NofiDanhMucThietBi { get; set; }
         public int NofiDeNghiTT { get; set; }
-        public event PropertyChangedEventHandler PropertyChangedEvent;
+        public event PropertyChangedEventHandler PropertyChanged;
         protected override void OnPropertyChanged([CallerMemberName] string propertyName = "")
         {
-            var changed = PropertyChangedEvent;
+            var changed = PropertyChanged;
             if (changed == null)
                 return;
 
